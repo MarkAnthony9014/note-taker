821836f83286e5358e58ea3485512d18522f9071
MarkAnthony9014 <markanthony9014@gmail.com>
2022-09-28 21:59:27 -0500

    added a read me

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Note Taker Starter Code
+
+## Description
+
+- Create paths for the HTML
+- Create GET routes for the API
+- Create a POST route for the API to add notes. 
+
+## Screenshot
+
+![Screenshot](./notesnip.jpg)
+
+
+## Github Deployment Link 
+
+[Github Link]()
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const { db } = require('./db/db');
 const fs = require('fs');
 const path = require('path');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3003;
 
 const app = express();
 
@@ -14,23 +15,64 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+const addNote = (body, dbArray) => {
+  const newNote = body;
+
+ dbArray.push(newNote);
+
+  fs.writeFileSync(path.join(__dirname, './db/db.json'),
+  JSON.stringify({ db: dbArray }, null, 2)
+  );
+
+  return newNote;
+};
+
 
 // routes for HTMl 
 
-app.get('notes', function(req, res) {
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"))
+});
+
+app.get('/notes', function(req, res) {
+    console.log('hello')
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"))
+// app.get('*', function(req, res){
+//     res.sendFile(path.join(__dirname, "./public/index.html"));
+// });
+
+// routes for API
+
+app.get('/api/db', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) throw err;
+      var notes = JSON.parse(data);
+      res.json(notes);
+    });
+  });
+  
+
+
+
+  app.post('/api/db', (req, res) => {
+
+    req.body.id = db.length.toString();
+
+    const newNote = addNote(req.body, db);
+
+    res.json(newNote);
 });
-
-app.get('*', function(req, res){
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
-
+    
+   
 
 app.listen(PORT, function(){
     console.log("Listening on PORT " + PORT);
 });
+
+
+
+
+
+
